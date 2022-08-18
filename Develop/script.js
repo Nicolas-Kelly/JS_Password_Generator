6c9cdfe142208b183b66aa37efe62cd52e845b1e
Nicolas Kelly <nicolas.kelly12321@yahoo.com>
2022-08-18 17:49:06 -0500

    fixed random math generator

--- Develop/script.js
+++ Develop/script.js
@@ -2,13 +2,17 @@
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
-function randomNumber(min, max) {
-  return Math.floor(Math.random()*(max - min) + min)
+function randomInt(min, max) {
+if (!max) {
+  max = min 
+  min = 0
 };
-function randomSelection(list) {
-  return list [randomNumber(0, list.length - 1)]
+var rand = Math.random()
+return Math.floor(min*(1 - rand) + rand*max)
 }
-
+function randomSelection(list) {
+  return list[randomInt(list.length)]
+};
 
 function generatePassword() {
   var input = window.prompt("How many characters in generated password?");
@@ -26,16 +30,16 @@ function generatePassword() {
 
 // USE window.confirm TO CREATE A WINDOW THAT NEEDS A YES OR NO.
   var lowerCaseConfirm = window.confirm("Include lowercase letters in generated password?")
-  var lowerCase = ("a b c d e f g h i j k l m n o p q r s t u v w x y z") // All lowercase characters
+  var lowerCase = ("abcdefghijklmnopqrstuvwxyz") // All lowercase characters
 
   var numberConfirm = window.confirm("Include numeric characters in generated password?");
-  var numberCase = ("0 1 2 3 4 5 6 7 8 9"); // All numbers characters
+  var numberCase = ("0123456789"); // All numbers characters
 
   var symbolsConfirm = window.confirm("Include symbols in generated password?");
-  var symbolsCase= ("! @ # $ % ^ & * ( ) _ +"); // All symbols characters
+  var symbolsCase= ("!@#$%^&*()_+"); // All symbols characters
 
   var upperCaseConfirm = window.confirm("Include uppercase letters in generated password?");
-  var upperCase = ("A B C D E F G H I J K L M N O P Q R S T U V W X Y Z"); // All uppercase characters
+  var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"); // All uppercase characters
 
   var passwordOptions = [];
   if (lowerCaseConfirm === true) { // If lowerCaseConfirm is true, then add lowerCase var into passwordOptions var
@@ -56,7 +60,6 @@ var generatedPassword = ""
 for (var i = 0; i < passwordLength; i++) { //Chooses a random number for password options as long as the length -1 (THE ARRAY STARTS AS 0 SO THE -1 IS NEEDED.)
   var randomList = randomSelection(passwordOptions)
   var randomCharacter = randomSelection(randomList)
-  console.log(randomCharacter)
 };
 
   // MAKE SURE TO USE console.log(passwordOptions); EVERY NOW AND THEN TO CHECK THAT THE ARRAYS ARE BEING ADDED TO THE passwordOptions var WHEN THE PROMPTS ARE ANSWERED.
