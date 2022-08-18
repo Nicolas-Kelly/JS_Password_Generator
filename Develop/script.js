1ee96d8a9ff1e7a92f71f3370e54137d4496d2e3
Nicolas Kelly <nicolas.kelly12321@yahoo.com>
2022-08-18 18:14:43 -0500

    New problem: 9, z, Z, and % are only appearing

--- Develop/script.js
+++ Develop/script.js
@@ -30,16 +30,16 @@ function generatePassword() {
 
 // USE window.confirm TO CREATE A WINDOW THAT NEEDS A YES OR NO.
   var lowerCaseConfirm = window.confirm("Include lowercase letters in generated password?")
-  var lowerCase = ("abcdefghijklmnopqrstuvwxyz") // All lowercase characters
+  var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"] // All lowercase characters
 
   var numberConfirm = window.confirm("Include numeric characters in generated password?");
-  var numberCase = ("0123456789"); // All numbers characters
+  var numberCase = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]; // All numbers characters
 
   var symbolsConfirm = window.confirm("Include symbols in generated password?");
-  var symbolsCase= ("!@#$%^&*()_+"); // All symbols characters
+  var symbolsCase= ["!", "@", "#", "$", "%"]; // All symbols characters
 
   var upperCaseConfirm = window.confirm("Include uppercase letters in generated password?");
-  var upperCase = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ"); // All uppercase characters
+  var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; // All uppercase characters
 
   var passwordOptions = [];
   if (lowerCaseConfirm === true) { // If lowerCaseConfirm is true, then add lowerCase var into passwordOptions var
@@ -54,14 +54,18 @@ function generatePassword() {
   if (upperCaseConfirm === true) { // If upperCaseConfirm is true, then add upperCase var into passwordOptions var
     passwordOptions.push(upperCase)
   }
+  if (passwordOptions.length === 0) { // Alert that appears when all of the question prompts come up false.
+    alert("YOU MUST SAY YES TO ONE OF THE GIVEN QUESTIONS!")
+  }
 
-var generatedPassword = ""
+var generatePassword = ""
 
 for (var i = 0; i < passwordLength; i++) { //Chooses a random number for password options as long as the length -1 (THE ARRAY STARTS AS 0 SO THE -1 IS NEEDED.)
   var randomList = randomSelection(passwordOptions)
-  var randomCharacter = randomSelection(randomList)
+  generatePassword += randomList
 };
 
+return generatePassword
   // MAKE SURE TO USE console.log(passwordOptions); EVERY NOW AND THEN TO CHECK THAT THE ARRAYS ARE BEING ADDED TO THE passwordOptions var WHEN THE PROMPTS ARE ANSWERED.
 }
 
